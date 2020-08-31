4fd581ff743a6575f030039cb9cb2610108405a1
Frederik Kristensen <fred009a@edu.easj.dk>
2020-08-31 11:22:38 +0200

    Add project files.

---
+++ Student.cs
@@ -0,0 +1,63 @@
+﻿using System;
+
+namespace BrushUpUnitTest
+{
+    public enum Gendertype
+    {
+        Cisgender, Transgender
+    }
+
+    public class Student
+    {
+        private string _name;
+        private string _address;
+        private int _semester;
+
+        public Student(string name, string address, int semester, Gendertype gender)
+        {
+            Name = name;
+            Address = address;
+            Semester = semester;
+            Gender = gender;
+        }
+
+        public string Name
+        {
+            get { return _name; }
+            set
+            {
+                if (value == null) throw new ArgumentNullException("name", "Remeber to enter name!");
+                if (value.Length < 2) throw new ArgumentOutOfRangeException("The name is too short");
+                _name = value;
+            }
+        }
+
+        public string Address 
+        {
+            get { return _address; }
+            set 
+            {
+                if (value == null) throw new ArgumentNullException("Address", "Remeber to enter a Address");
+                _address = value;
+            } 
+        }
+
+        public int Semester
+        {
+            get { return _semester; }
+            set
+            {
+                if (value < 1) throw new ArgumentOutOfRangeException("Semester is too low");
+                if (value > 8) throw new ArgumentOutOfRangeException("Semester is too high");
+                _semester = value;
+            }
+        }
+
+        public Gendertype Gender { get; set; }
+
+        public override string ToString()
+        {
+            return base.ToString();
+        }
+    }
+}
---
+++ Teacher.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BrushUpUnitTest
+{
+    public enum Gender
+    {
+        Cisgender, Transgender
+    }
+
+    class Teacher
+    {
+        private string _name;
+        private string _address;
+        private int _salary;
+
+        public Teacher()
+        {
+
+        }
+
+        public string Name
+        {
+            get { return _name; }
+            set
+            {
+                _name = value; 
+            }
+        }
+
+        public string Address
+        {
+            get { return _address; }
+            set
+            {
+                _address = value;
+            }
+        }
+
+        public int Salary
+        {
+            get { return _salary; }
+            set
+            {
+                _salary = value;
+            }
+        }
+    }
+}
